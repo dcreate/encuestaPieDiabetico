85a8e6bdaecaf0771a883fb42839a97792f760a1
Felix de Jesus Carrillo Celerino <dcreatedark@gmail.com>
2014-11-19 18:36:51 -0600

    cargar modulo para loguearse
    Modulo que presenta la manera de loguearse para entrar a un menu oculto

---
+++ login.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Encuesta para tesis, Pie Diabeico" />
+	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1 /">
+	<title>ENCUESTA PIE DIABETICO</title>
+	<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC|Istok+Web|Esteban|Open+Sans' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="normalize.css" />
+	<link rel="stylesheet" href="estilo.css" />
+	<script src="prefixfree.js"></script>
+</head>
+<body>
+	<header>
+		<figure id="logo">
+			<img src="image/logo.jpg" />
+		</figure>
+		<h1>Encuesta Sobre Pie Diabetico</h1>
+		<figure id="avatar">
+			<img src="image/pie.jpg" />
+		</figure>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="index.php">ENCUESTA</a></li>
+			<li><a href="contenido.php">RESULTADOS</a></li>
+		</ul>
+	</nav>
+	<?php
+	if(!empty($_GET['msj']))
+	{
+	?>
+		<aside>
+			<?php echo "<h4>".$_GET['msj']."</h4>";?>
+		</aside>
+	<?php
+	}
+	?>
+	<section id="contenido">
+		<article class="item">
+			<form id="formulario" class="formulario" method="post" action="logue.php">
+				<label for="user">USUARIO: </label>
+				<input type="text" name="user"  placerholder="NOMBRE USUARIO" />
+				<?php if(!empty($_GET['user'])){echo "<h4>". $_GET['user']."</h4>";}?>
+				<label for="pass">PASSWORD:</label>
+				<input type="password" name="pass" /><br>
+				<?php if(!empty($_GET['pas'])){echo "<h4>". $_GET['pas']."</h4>";}?>
+				<input type="submit" name="enviar" value="ACCESAR">
+			</form>
+		</article>
+	</section>
+	<footer>
+		<p>
+			<strong>Encuesta para obtener datos sobre pie Diabetico</strong>
+		</p>
+		<p>
+			Creado por @dcreate
+		</p>
+	</footer>
+</body>
+</html>
